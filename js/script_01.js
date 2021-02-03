fde2db99e14235589733d27c4986098b84121d02
moonjongjs <moonseonjong@naver.com>
2021-02-03 11:21:47 +0900

    Upload 2021-02-03 11:21

---
+++ js/script_01.js
@@ -0,0 +1,8 @@
+//script_01.js
+//자바스크립트 처음 코딩하기
+//ECMA Script 5
+console.log('ECMA Script 5 자바스크립트 처음 코딩하기 & node로 실행하기!!!');
+
+//ECMA Script 6
+console.log(`ECMA Script 6 자바스크립트 처음 코딩하기 & node로 실행하기!!!`);
+
